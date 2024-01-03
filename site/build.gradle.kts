6f439d2dfc0e79488ec5175ac31d484c81257f5f
kjeller <kallestralman@gmail.com>
2024-01-03 23:23:50 +0100

    WIP generate rss and start working on fix markdown content

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -6,6 +6,7 @@ import org.commonmark.ext.front.matter.YamlFrontMatterBlock
 import org.commonmark.ext.front.matter.YamlFrontMatterVisitor
 import org.commonmark.node.AbstractVisitor
 import org.commonmark.node.CustomBlock
+import java.util.Date
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
@@ -63,7 +64,14 @@ class MarkdownVisitor : AbstractVisitor() {
     }
 }
 
-data class MarkdownEntry(
+data class FrontMatterKeys(
+    val author: String = "author",
+    val date: String = "date",
+    val title: String = "title",
+    val tags: String = "tags",
+)
+
+data class MarkdownData(
     val file: File,
     val date: String?,
     val title: String? = null,
@@ -71,9 +79,43 @@ data class MarkdownEntry(
     val tags: List<String>? = emptyList()
 )
 
-fun String.escapeQuotes() = this.replace("\"", "\\\"")
+data class RssData(
+    val title: String,
+    val baseUrl: String,
+    val author: String,
+    val description: String,
+    val language: String,
+)
+
+fun getUrlFromFilePath(file: File) =
+    file
+        .path
+        .substringAfterLast("posts")
+        .substringBefore("\\index.md") // Windows
+        .substringBefore("/index.md") // Unix
+        .substringBefore(".md")
+        .lowercase()
+        .invariantSeparatorsPath
 
 val markdownResourceDir = layout.projectDirectory.dir("src/jsMain/resources/markdown/posts")
+val fmk = FrontMatterKeys()
+val parser = kobweb.markdown.features.createParser()
+val markdownEntries: List<MarkdownData> =
+    markdownResourceDir.asFileTree.filter { it.extension == "md" }.map {
+        val visitor = MarkdownVisitor()
+        parser
+            .parse(it.readText())
+            .accept(visitor)
+        val fm = visitor.frontMatter
+        MarkdownData(
+            file = it,
+            title = fm[fmk.title]?.firstOrNull(),
+            author = fm[fmk.author]?.firstOrNull(),
+            tags = fm[fmk.tags],
+            date = fm[fmk.date]?.firstOrNull()
+        )
+    }
+
 val generateMarkdownEntriesTask = task("generateMarkdownEntries") {
     val group = "dev/stralman"
     val genDir = layout.buildDirectory.dir("generated/$group/src/jsMain/kotlin").get()
@@ -85,22 +127,6 @@ val generateMarkdownEntriesTask = task("generateMarkdownEntries") {
         .withPropertyName("generatedMarkdownEntryData")
 
     doLast {
-        val parser = kobweb.markdown.features.createParser()
-        val markdownEntries: List<MarkdownEntry> =
-            markdownResourceDir.asFileTree.filter { it.extension == "md" }.map {
-                val visitor = MarkdownVisitor()
-                parser
-                    .parse(it.readText())
-                    .accept(visitor)
-                val fm = visitor.frontMatter
-                MarkdownEntry(
-                    file = it,
-                    title = fm["title"]?.firstOrNull(),
-                    author = fm["author"]?.firstOrNull(),
-                    tags = fm["tags"],
-                    date = fm["date"]?.firstOrNull()
-                )
-            }
         genDir.file("articles.kt").asFile.apply {
             parentFile.mkdirs()
             writeText(buildString {
@@ -119,17 +145,7 @@ val generateMarkdownEntriesTask = task("generateMarkdownEntries") {
                 markdownEntries.sortedByDescending { it.date }.forEach { entry ->
                     appendLine(
                         """ MarkdownEntry(
-                            |       path = "/posts${
-                            entry
-                                .file
-                                .path
-                                .substringAfterLast("posts")
-                                .substringBefore("\\index.md") // Windows
-                                .substringBefore("/index.md") // Unix
-                                .substringBefore(".md")
-                                .lowercase()
-                                .invariantSeparatorsPath
-                        }",
+                            |       path = "/posts${getUrlFromFilePath(entry.file)}",
                             |       date = "${entry.date?.ifEmpty { "1970-01-01" }}".toLocalDate(),
                             |       title = "${entry.title}",
                             |       author = "${entry.author}",
@@ -140,7 +156,89 @@ val generateMarkdownEntriesTask = task("generateMarkdownEntries") {
                 }
                 appendLine(")")
             })
-            println("Generated ${absolutePath}")
+            println("Generated $absolutePath")
+        }
+    }
+}
+val generateRssFromMarkdownEntriesTask = task("generateRssFromMarkdownEntries") {
+    val group = "dev/stralman"
+    val genDir = layout.buildDirectory.dir("processedResources/js/main/public").get()
+    val rssData = RssData(
+        title = "stralman.dev",
+        baseUrl = "https://stralman.dev",
+        author = "Karl Strålman",
+        description = "TODO",
+        language = "en-us",
+    )
+    inputs.dir(markdownResourceDir)
+        .withPropertyName("markdownEntries")
+        .withPathSensitivity(PathSensitivity.RELATIVE)
+    outputs.dir(genDir)
+        .withPropertyName("rssEntrydata")
+
+    doLast {
+        //SimpleDateFormat df = new SimpleDateFormat ("MM-dd-yyyy_hh-mm")
+        val buildDate = Date()
+        genDir.file("index.xml").asFile.apply {
+            parentFile.mkdirs()
+            writeText(buildString {
+                appendLine(
+                    """<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+                    |<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+                    |<channel>
+                    |   <title>${rssData.title}</title>
+                    |   <link>${rssData.baseUrl}</link>
+                    |   <description>${rssData.description}</description>
+                    |   <generator>Kobweb -- kobweb.varabyte.com</generator>
+                    |   <language>${rssData.language}</language>
+                    |   <copyright>© 2023, ${rssData.author}</copyright>
+                    |   <lastBuildDate>${buildDate}</lastBuildDate>
+                    |   <atom:link href="${rssData.baseUrl}/index.xml" rel="self" type="application/rss+xml"/>
+                    """.trimMargin()
+                )
+                markdownEntries.forEach {
+                    val url = "${rssData.baseUrl}/posts${getUrlFromFilePath(it.file)}"
+                    appendLine(
+                        """<item>
+                        |   <title>${it.title}</title>
+                        |   <link>${url}</link>
+                        |   <guid>${url}</guid>
+                        |   <pubDate>${it.date}</pubDate>
+                        |   <description>TODO</description>
+                        |</item>
+                        """.trimMargin()
+                    )
+                }
+                appendLine(
+                    """
+                    |</channel>
+                    |</rss> 
+                    """.trimMargin()
+                )
+            })
+            println("Generated $absolutePath")
+        }
+    }
+}
+val copyMarkdownResourcesTask = task("copyMarkdownResources") {
+    val group = "dev/stralman"
+    val genDir = layout.buildDirectory.dir("processedResources/js/main/public").get()
+
+    inputs.dir(markdownResourceDir)
+        .withPropertyName("markdownEntries")
+        .withPathSensitivity(PathSensitivity.RELATIVE)
+    outputs.dir(genDir)
+        .withPropertyName("markdownResources")
+
+    println("HEPOAKSKAPOSDPASDPOKSAd")
+
+    doLast {
+        markdownEntries.forEach {
+            copy {
+                from("${it.file.parentFile}")
+                into("$genDir")
+                exclude("*.md")
+            }
         }
     }
 }
@@ -149,7 +247,7 @@ kotlin {
     // This example is frontend only. However, for a fullstack app, you can uncomment the includeServer parameter
     // and the `jvmMain` source set below.
     configAsKobwebApplication("stralman" /*, includeServer = true*/)
-
+    generateRssFromMarkdownEntriesTask
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -162,14 +260,14 @@ kotlin {
                 implementation(compose.html.core)
                 implementation(libs.kobweb.core)
                 implementation(libs.kobweb.silk)
-                // This default template uses built-in SVG icons, but what's available is limited.
-                // Uncomment the following if you want access to a large set of font-awesome icons:
                 implementation(libs.silk.icons.fa)
                 implementation(libs.kobwebx.markdown)
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
             }
 
             kotlin.srcDir(generateMarkdownEntriesTask)
+            resources.srcDir(generateRssFromMarkdownEntriesTask)
+            copyMarkdownResourcesTask
         }
 
         // Uncomment the following if you pass `includeServer = true` into the `configAsKobwebApplication` call.
