a0e4ce45570ead956c6c99f71ea7b6cb599d3d8c
Kalle Strålman <kallestralman@gmail.com>
2024-01-09 00:39:31 +0100

    Add hover color for index markdown entries

--- site/src/jsMain/kotlin/dev/stralman/pages/Index.kt
+++ site/src/jsMain/kotlin/dev/stralman/pages/Index.kt
@@ -3,17 +3,19 @@ package dev.stralman.pages
 import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.css.TextAlign
 import com.varabyte.kobweb.compose.css.VerticalAlign
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
 import com.varabyte.kobweb.compose.ui.modifiers.color
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
 import com.varabyte.kobweb.compose.ui.modifiers.fontSize
 import com.varabyte.kobweb.compose.ui.modifiers.lineHeight
 import com.varabyte.kobweb.compose.ui.modifiers.margin
-import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
 import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.compose.ui.modifiers.textAlign
 import com.varabyte.kobweb.compose.ui.modifiers.verticalAlign
@@ -22,6 +24,7 @@ import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.silk.components.navigation.Link
 import com.varabyte.kobweb.silk.components.style.ComponentStyle
 import com.varabyte.kobweb.silk.components.style.base
+import com.varabyte.kobweb.silk.components.style.hover
 import com.varabyte.kobweb.silk.components.style.toModifier
 import dev.stralman.articles.markdownEntries
 import dev.stralman.components.layouts.PageLayout
@@ -30,7 +33,6 @@ import dev.stralman.util.getShortMonth
 import org.jetbrains.compose.web.css.Color
 import org.jetbrains.compose.web.css.cssRem
 import org.jetbrains.compose.web.css.px
-import org.jetbrains.compose.web.css.vw
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
 
@@ -44,49 +46,68 @@ val BlogPostEntry by ComponentStyle.base {
         .textAlign(TextAlign.Right)
 }
 
+val BlogPostRow by ComponentStyle {
+    base {
+        Modifier
+            .backgroundColor(Color("#2b2a33"))
+            .borderRadius(0.25.cssRem)
+            .fillMaxWidth()
+            .padding(5.px)
+    }
+    hover {
+        Modifier
+            .backgroundColor(Color("#42414d"))
+    }
+}
+
 @Composable
 fun BlogPostList(
     modifier: Modifier = Modifier
 ) {
     Column {
         markdownEntries.forEach {
-            Column(
+            /*Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = modifier
                     .maxWidth(65.vw)
+                    .minWidth(35.vw)
                     .margin(bottom = 15.px),
+            ) {*/
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start,
+                modifier = BlogPostRow.toModifier()
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    BadgeContent {
-                        Text("${getShortMonth(it.date.month)} ${
+                BadgeContent {
+                    Text(
+                        "${getShortMonth(it.date.month)} ${
                             it.date.dayOfMonth.toString().padStart(2, '0')
-                        } ${it.date.year}")
-                    }
-                    Column {
-                        Link(
-                            text = it.title,
-                            path = it.path,
-                            modifier = BlogPostEntry
-                                .toModifier()
-                                .then(
-                                    modifier
-                                        .textAlign(TextAlign.Left)
-                                        .verticalAlign(VerticalAlign.TextTop)
-                                )
-                        )
-                        P(
-                            BlogPostEntry.toModifier()
-                                .then(
-                                    modifier
-                                        .fontSize(0.7.cssRem)
-                                        .textAlign(TextAlign.Left)
-                                        .color(Color("#6c757d"))
-                                )
-                                .toAttrs()
-                        ) {
-                            Text(it.tags.joinToString(", "))
-                        }
+                        } ${it.date.year}"
+                    )
+                }
+                Column {
+                    Link(
+                        text = it.title,
+                        path = it.path,
+                        modifier = BlogPostEntry
+                            .toModifier()
+                            .then(
+                                modifier
+                                    .textAlign(TextAlign.Left)
+                                    .verticalAlign(VerticalAlign.TextTop)
+                            )
+                    )
+                    P(
+                        BlogPostEntry.toModifier()
+                            .then(
+                                modifier
+                                    .fontSize(0.7.cssRem)
+                                    .textAlign(TextAlign.Left)
+                                    .color(Color("#6c757d"))
+                            )
+                            .toAttrs()
+                    ) {
+                        Text(it.tags.joinToString(", "))
                     }
                 }
             }
